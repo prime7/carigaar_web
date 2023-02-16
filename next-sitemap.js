8cebe675ae3b2008c11288fa3d6922240dae5bd9
Tarek Ahmed <tarek5701@gmail.com>
2023-02-16 15:54:21 -0700

    added sitemap setting

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: "https://carigaar.com",
+  generateRobotsTxt: true,
+  // optional
+  // robotsTxtOptions: {
+  //   additionalSitemaps: [
+  //     'https://carigaar.com/my-custom-sitemap-1.xml',
+  //   ],
+  // },
+};
