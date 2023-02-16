d6390fe8de821187c9c7c3a069f67f078b5424f7
Tarek Ahmed <tarek5701@gmail.com>
2023-02-16 16:10:09 -0700

    image optimization added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
@@ -5,13 +8,16 @@ const withMDX = require("@next/mdx")({
     rehypePlugins: [],
   },
 });
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "md", "mdx"],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
-  reactStrictMode: true,
-});
+module.exports = withPlugins(
+  [optimizedImages],
+  withMDX({
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
+    reactStrictMode: true,
+  })
+);
