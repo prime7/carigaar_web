58f5abf81280f789e35dd708135ca186c377da54
Tarek Ahmed <tarek5701@gmail.com>
2023-04-10 23:22:31 -0700

    added locals and gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 import { MantineProvider } from "@mantine/core";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,6 +12,19 @@ export default function App({ Component, pageProps }) {
           colorScheme: "light",
         }}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10816289133"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-10816289133');
+        `}
+        </Script>
         <Component {...pageProps} />
         <TawkMessengerReact
           propertyId="62aead82b0d10b6f3e78139d"
