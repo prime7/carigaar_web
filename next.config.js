bca455687c3f963617af579c45fae08456b2152d
Tarek Ahmed <tarek5701@gmail.com>
2023-07-18 20:36:36 -0700

    removed mdx

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-module.exports = withPlugins(
-  [optimizedImages],
-  withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx"],
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback.fs = false;
-      }
-      return config;
-    },
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-  })
-);
+module.exports = withPlugins([optimizedImages]);
