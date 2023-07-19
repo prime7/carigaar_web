2e6e6ee0f58b8dc14b404a6fce9be35278598483
Tarek Ahmed <tarek5701@gmail.com>
2023-07-18 20:56:13 -0700

    fx

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
-const nextConfig = {};
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = nextConfig;
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+module.exports = withPlugins(
+  [optimizedImages],
+  withMDX({
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+  })
+);
